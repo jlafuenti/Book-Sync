5e4e0018c45f543fa3d55e23dc435b4c80b4e1e4
Jesse <25994131+jlafuenti@users.noreply.github.com>
2026-03-13 22:06:14 -0400

    feat: Add AudioTranscript model and integrate caching in transcription pipeline

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -2,6 +2,7 @@
 
 from .book import EBook, AudioBook, BookPair, PairStatus
 from .sync_map import SyncMap, SyncPoint
+from .transcript import AudioTranscript
 from .user import User
 from .bookmark import Bookmark
 from .progress import UserProgress, ProgressType
